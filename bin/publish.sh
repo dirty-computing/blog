9356dcdab42d593e4094c700a92c3866e6f4a306
Jefferson Quesado <jeff.quesado@gmail.com>
2021-12-28 12:42:15 -0300

    Script para mover de `/_drafts/` para `/_posts/`
    Ainda bem porquinho e incipiente, mas funcional

---
+++ bin/publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+normaliza_draft() {
+	local DRAFT="$1"
+	if [ "${DRAFT%/*}" = "$DRAFT" ]; then
+		DRAFT="_drafts/$DRAFT"
+		if [ "${DRAFT##*.}" = "$DRAFT" ]; then
+			DRAFT+=.md
+		fi
+	elif [ "${DRAFT%/*}" != "_drafts" ]; then
+		echo "Deu ruim, não começa com '_drafts' ou tem mais de uma barra" >&2
+		exit 1
+	fi
+	echo "$DRAFT"
+}
+
+if [ $# != 1 ]; then
+	echo "Forneça um (e apenas um) draft para publicar" >&2
+	exit 1
+fi
+
+DRAFT="`normaliza_draft "$1"`"
+
+if [ ! -f "$DRAFT" ]; then
+	echo "Não existe '$DRAFT'" >&2
+	exit 1
+fi
+
+git add "$DRAFT"
+
+POST="_posts/`date --iso`-${DRAFT#_drafts/}"
+
+git mv -v "$DRAFT" "$POST"
