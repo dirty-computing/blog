9da65618e2db3cfd0338d6bf136d89162c4f5ab2
Jefferson Quesado <jeff.quesado@gmail.com>
2025-03-07 03:06:07 -0300

    Página gerencial

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -95,6 +95,129 @@ module Computaria
         end
     end
 
+
+
+          class PostComparer
+            MATCHER = %r!^(.+/)*(\d+-\d+-\d+)-(.*)$!.freeze
+      
+            attr_reader :path, :date, :slug, :name
+      
+            def initialize(name)
+              @name = name
+      
+              all, @path, @date, @slug = *name.sub(%r!^/!, "").match(MATCHER)
+              unless all
+                raise Jekyll::Errors::InvalidPostNameError,
+                      "'#{name}' does not contain valid date and/or title."
+              end
+      
+              basename_pattern = "#{date}-#{Regexp.escape(slug)}\\.[^.]+"
+              @name_regex = %r!^_posts/#{path}#{basename_pattern}|^#{path}_posts/?#{basename_pattern}!
+            end
+      
+            def post_date
+              @post_date ||= Jekyll::Utils.parse_date(
+                date,
+                "'#{date}' does not contain valid date and/or title."
+              )
+            end
+      
+            def ==(other)
+              other.relative_path.match(@name_regex)
+            end
+      
+            def deprecated_equality(other)
+              slug == post_slug(other) &&
+                post_date.year  == other.date.year &&
+                post_date.month == other.date.month &&
+                post_date.day   == other.date.day
+            end
+      
+            private
+      
+            # Construct the directory-aware post slug for a Jekyll::Post
+            #
+            # other - the Jekyll::Post
+            #
+            # Returns the post slug with the subdirectory (relative to _posts)
+            def post_slug(other)
+              path = other.basename.split("/")[0...-1].join("/")
+              if path.nil? || path == ""
+                other.data["slug"]
+              else
+                "#{path}/#{other.data["slug"]}"
+              end
+            end
+          end
+      
+          class PostUrlWA < Liquid::Tag
+            include Jekyll::Filters::URLFilters
+      
+            def initialize(tag_name, post, tokens)
+              super
+              @orig_post = post.strip
+              #begin
+              #  @post = PostComparer.new(@orig_post)
+              #rescue StandardError => e
+              #  raise Jekyll::Errors::PostURLError, <<~MSG
+              #    Could not parse name of post "#{@orig_post}" in tag 'post_url'.
+              #     Make sure the post exists and the name is correct.
+              #     #{e.class}: #{e.message}
+              #  MSG
+              #end
+            end
+      
+            def render(context)
+              @context = context
+              liquid_solved_orig_post = Liquid::Template.parse(@orig_post).render(context)
+
+              if liquid_solved_orig_post == @orig_post
+                post_from_input_string = "\"#{liquid_solved_orig_post}\""
+              else
+                post_from_input_string = "\"#{liquid_solved_orig_post}\" (from input \"#{@orig_post}\")"
+              end
+              begin
+                post = PostComparer.new(liquid_solved_orig_post)
+              rescue StandardError => e
+                raise Jekyll::Errors::PostURLError, <<~MSG
+                  Could not parse name of post #{post_from_input_string} in tag 'post_url'.
+                   Make sure the post exists and the name is correct.
+                   #{e.class}: #{e.message}
+                MSG
+              end
+
+              #print "post_path #{post_path}\n"
+              #print "post #{post}\n"
+              #print "---------\n"
+
+              site = context.registers[:site]
+      
+              site.posts.docs.each do |document|
+                return relative_url(document) if post == document
+              end
+      
+              # New matching method did not match, fall back to old method
+              # with deprecation warning if this matches
+      
+              site.posts.docs.each do |document|
+                next unless post.deprecated_equality document
+      
+                Jekyll::Deprecator.deprecation_message(
+                  "A call to '{% post_url #{post.name} %}' did not match a post using the new " \
+                  "matching method of checking name (path-date-slug) equality. Please make sure " \
+                  "that you change this tag to match the post's name exactly."
+                )
+                return relative_url(document)
+              end
+      
+              raise Jekyll::Errors::PostURLError, <<~MSG
+                Could not find post #{post_from_input_string} in tag 'post_url'.
+                Make sure the post exists and the name is correct.
+              MSG
+            end
+          end
+
+
     private
 
     def self.reverse_alias_tag(context)
@@ -124,3 +247,4 @@ module Computaria
 end
 
 Liquid::Template.register_filter(Computaria::TagNormalizer)
+Liquid::Template.register_tag("post_urlwa", Computaria::PostUrlWA)
---
+++ managerial.md
@@ -0,0 +1,96 @@
+---
+layout: page
+title: Gerencial
+permalink: /managerial/
+show: true
+---
+
+<style>
+    table.lalala {
+        border: 1px solid gray;
+        width: 90%
+    }
+    table.lalala tr {
+        /* width: 90% */
+    }
+    table.lalala th {
+        border: 1px solid gray;
+        font-weight: bold;
+    }
+    table.lalala td {
+        border: 1px solid gray;
+    }
+</style>
+
+Ambiente Jekyll que gerou essa página? `{{jekyll.environment}}`
+
+<table class='lalala'>
+    <tr>
+        <th>Variável</th><th>Descrição</th><th>Valor</th><th>Toggle</th><th>Posts</th>
+    </tr>
+    <tbody>
+        {% for var in site.data.meta.variables -%}
+        {% if var.env == '*' or var.env == jekyll.environment %}
+        <tr>
+            <td>{{ var.name }}/{{ var.env }}</td><td>{{ var.description }}</td>
+            <td id="td-{{ var.name }}"></td>
+            <td><button onclick="alterarStorage('{{ var.name }}')">Alterar</button></td>
+<td markdown="1">
+{% if var.posts.size > 0 %}
+{%- for bg_post in var.posts -%}
+- [{{ bg_post.title }}]({% post_urlwa {{ bg_post.slug }} %})
+{% endfor %}
+{% endif %}
+</td>
+        </tr>
+        {% endif %}
+        {% endfor %}
+    </tbody>
+</table>
+
+Links para meta informação (post citando):
+
+{% for meta_link in site.data.meta.links %}
+- {{ meta_link.link }}
+{%- for bg_post in meta_link.posts %}
+  - [{{ bg_post.title }}]({% post_urlwa {{ bg_post.slug }} %})
+{%- endfor %}
+{% endfor %}
+
+<script>
+const varaiveisStorage = [
+{% for var in site.data.meta.variables -%}
+{% unless forloop.first %} , {% endunless %}
+"{{var.name}}"
+{% endfor %}
+]
+
+function toTdId(varName) {
+    return "td-" + varName
+}
+
+function alterarStorage(varName) {
+    const currValue = localStorage.getItem(varName)
+    const newValue = (currValue === "true")? "false": "true"
+    localStorage.setItem(varName, newValue)
+
+    const id = toTdId(varName)
+    const element = document.getElementById(id)
+    if (!!element) {
+        element.innerText = newValue
+    }
+}
+
+function populateVar(varName) {
+    const id = toTdId(varName)
+    const element = document.getElementById(id)
+    if (!!element) {
+        const varValue = localStorage.getItem(varName)
+        element.innerText = varValue
+    }
+}
+
+for (const varName of varaiveisStorage) {
+    populateVar(varName)
+}
+</script>
